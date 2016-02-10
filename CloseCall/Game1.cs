7ffef2808a328cc72d7681bad63dc93b767111cf
KokoDoko <benni_jolink@hotmail.com>
2016-02-10 22:49:10 +0100

    base.Update en base.Draw verwijderd

--- CloseCall/Game1.cs
+++ CloseCall/Game1.cs
@@ -21,14 +21,11 @@ namespace CloseCall
         }
 
         /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
+        /// Hier plaats je code die alleen bij het opstarten van de game uitgevoerd hoeft te worden
+        /// Let op: base.Initialize() roept LoadContent() aan
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             instance = this;
             _world = new World();
 
@@ -48,49 +45,26 @@ namespace CloseCall
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-            
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
         }
 
         /// <summary>
         /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
         /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
             _world.Update();
-
-            base.Update(gameTime);
         }
 
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
             spriteBatch.Begin();
             _world.Draw();
             spriteBatch.End();
-            base.Draw(gameTime);
         }
     }
 }
